cd23029c255ca8083decb3920e060bb4e3c9b47f
Joshua Lee <joshslee93@gmail.com>
2019-01-08 11:44:24 -0800

    Dockered

--- server/server.js
+++ server/server.js
@@ -5,14 +5,13 @@ const port = process.env.PORT || 3000;
 const proxy = require('http-proxy-middleware');
 
 app.use(morgan('dev'));
-// app.use(express.static('./public'));
 app.use('/rooms/:roomid', express.static('./public'));
 
-app.use('/rooms/:id/photos', proxy({target: 'http://localhost:1337'}));
-app.use('/api/reviews/rooms/:roomid', proxy({target: 'http://localhost:3124'}));
-app.use('/api/ratings/rooms/:roomid', proxy({target: 'http://localhost:3124'}));
-app.use('/api/rooms/:id', proxy({target: 'http://localhost:8080'}));
-app.use('/house', proxy({target: 'http://localhost:3123'}));
+app.use('/rooms/:id/photos', proxy({target: 'http://54.175.98.175/'}));
+app.use('/api/reviews/rooms/:roomid', proxy({target: 'http://54.202.111.150'}));
+app.use('/api/ratings/rooms/:roomid', proxy({target: 'http://54.202.111.150'}));
+app.use('/api/rooms/:id', proxy({target: 'http://54.67.99.254/'}));
+app.use('/house', proxy({target: 'http://18.223.185.89'}));
 
 app.listen(port, () => {
   console.log(`server running at: htttp://localhost:${port}`);
