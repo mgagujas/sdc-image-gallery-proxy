c7305f558ba64092345a0ae54120277887e281f2
Michael Gagujas <mikey@Michaels-MacBook-Pro-2.local>
2019-01-25 13:36:37 -0800

    Proxy for deployment

--- server/server.js
+++ server/server.js
@@ -1,18 +1,28 @@
+require('newrelic')
 const express = require('express');
 const morgan = require('morgan');
+const proxy = require('http-proxy-middleware');
 const app = express();
 const port = process.env.PORT || 3000;
-const proxy = require('http-proxy-middleware');
+
 
 app.use(morgan('dev'));
-app.use('/rooms/:roomid', express.static('./public'));
 
-app.use('/rooms/:id/photos', proxy({target: 'http://54.175.98.175/'}));
-app.use('/api/reviews/rooms/:roomid', proxy({target: 'http://54.202.111.150'}));
-app.use('/api/ratings/rooms/:roomid', proxy({target: 'http://54.202.111.150'}));
-app.use('/api/rooms/:id', proxy({target: 'http://54.67.99.254/'}));
-app.use('/house', proxy({target: 'http://18.223.185.89'}));
+// Reviews - James
+app.use('/api/reviews/rooms/:roomid', proxy({target: 'http://localhost:3124'}));
+app.use('/api/ratings/rooms/:roomid', proxy({target: 'http://localhost:3124'}));
+
+// Image Gallery - Josh
+app.use('/rooms/:id/photos', proxy({target: 'http://localhost:1337'}));
+
+// Booking - Kevin
+app.use('/api/rooms/:id', proxy({target: 'http://localhost:8080'}));
+
+// Recommendations - Eric
+app.use('/house', proxy({target: 'http://localhost:3123'}));
+
+app.use('/rooms/:roomid', express.static("./public"));
 
 app.listen(port, () => {
-  console.log(`server running at: htttp://localhost:${port}`);
+  console.log(`server running at: http://localhost:${port}`);
 });
